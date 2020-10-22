26b9b0c4343884f591e4abca8bc5f046680e4a0d
laihuanjie <laihuanjie@ruijiating.com>
2020-10-22 14:57:54 +0800

    【updata】vue使用rsa分段加解密

---
+++ rsaencrypt/src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <el-input type="textarea" autosize v-model="string"></el-input>
+    <el-button type="success" @click='encrypt'>加密</el-button>
+    <el-input type="textarea" autosize v-model="encryptStr"></el-input>
+    <el-button type="danger" @click="decrypt">解密</el-button>
+    <el-input type="textarea" autosize v-model="decryptStr" :disabled='true'></el-input>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import { rsaEncrypt,rsaDecrypt } from "@/utils/crypt"
+
+export default {
+  name: 'Home',
+  data(){
+    return {
+      string:'',
+      encryptStr:'',
+      decryptStr:''
+    }
+  },
+  methods:{
+    encrypt(){
+      let self = this
+      self.encryptStr = rsaEncrypt(self.string)
+    },
+    decrypt(){
+      let self = this
+      self.decryptStr = rsaDecrypt(self.encryptStr)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
