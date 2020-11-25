d9f6e618157c21e2a0457820aff78bce3e2b2bbe
laihuanjie <laihuanjie@ruijiating.com>
2020-11-25 09:26:43 +0800

    【updata】更新

--- amapvue/src/main.js
+++ amapvue/src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import AmapVue from '@amap/amap-vue'
+import BaiduMap from 'vue-baidu-map'
 
 Vue.config.productionTip = false
+AmapVue.config.veusion = '2.0'
+AmapVue.config.key = '353a37a3af7c679221180d934a3fb597'
+Vue.use(AmapVue)
+Vue.use(BaiduMap,{
+  ak:'lNfsWmrS61GYzsrGwELhFpH5tVzx0pgb'
+})
 
 new Vue({
   router,
