288379be38ed87e9de88acaa7204e92594d91893
llhhj <2661163175@qq.com>
2021-01-06 10:27:24 +0800

    【uodate】路由嵌套

---
+++ sidebarcontrol/src/components/Layout/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="navBar"></div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="less" scoped>
+    .navBar{
+        width: 100%;
+        height: 50px;
+        background-color: aqua;
+    }
+</style>
--- sidebarcontrol/src/router/index.js
+++ sidebarcontrol/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Layout from "@/components/Layout/AppHeader.vue"
 
 Vue.use(VueRouter)
 
@@ -8,15 +9,16 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: Layout,
+    children:[{
+      path: 'home',
+      name: 'Home',
+      component: Home
+    },{
+      path: 'about',
+      name: 'About',
+      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }]
   }
 ]
 
