fd59cc9fc149a32fb440a5521ee8ebd09a29e2d8
llhhj <2661163175@qq.com>
2021-01-12 10:00:18 +0800

    【update】多参数监听

---
+++ computed/src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="type++">{{type}}</button>
+    <button @click="zoneCoding++">{{zoneCoding}}</button>
+    {{chartData}}
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+  data(){
+    return {
+      type:1,
+      zoneCoding:2
+    }
+  },
+  computed:{
+    chartData(){
+      console.log(1);
+      return {
+        type: this.type,
+        zoneCoding: this.zoneCoding
+      }
+    }
+  },
+  watch:{
+    chartData(value){
+      console.log(value);
+    }
+  }
+};
+</script>
