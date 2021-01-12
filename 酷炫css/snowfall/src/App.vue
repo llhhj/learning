c34bcd7b4ea54432de52a91d68967a6b5ec9b540
llhhj <2661163175@qq.com>
2021-01-12 09:48:26 +0800

    【update】snowFall

---
+++ 酷炫css/snowfall/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="scss">
+body{
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  background:  radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
+  height: 100vh;
+  overflow: hidden;
+  filter: drop-shadow(0 0 10px white);
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
