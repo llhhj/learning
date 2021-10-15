6fd287a629e584f0afbbe40061a13e02637c1515
赖焕杰 <9745391+lai-huanjie@user.noreply.gitee.com>
2021-10-15 08:58:11 +0800

    更新

---
+++ javascript/flat.js
@@ -0,0 +1,11 @@
+const deps = {
+    '采购部':[1,2,3],
+    '人事部':[5,8,12],
+    '行政部':[5,14,79],
+    '运输部':[3,64,105],
+}
+let member = Object.values(deps).flat(Infinity);
+
+console.log(`a`, member)
+
+let a = obj?.name
